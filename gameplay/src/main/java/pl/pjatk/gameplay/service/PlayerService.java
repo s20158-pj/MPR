8e099f2d69ace46b2cb7762ef843ca02d01f13f0
Patryk Klimek <s20158@pjwstk.edu.pl>
2020-10-24 14:22:33 +0200

    Created controller, service, and model of player.

---
+++ gameplay/src/main/java/pl/pjatk/gameplay/service/PlayerService.java
@@ -0,0 +1,19 @@
+package pl.pjatk.gameplay.service;
+
+import org.springframework.stereotype.Service;
+import pl.pjatk.gameplay.model.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PlayerService {
+
+    public List<Player> findAll(){
+        List<Player> players = new ArrayList<>();
+        Player player = new Player("Dziki swinior", 100, 69, 5);
+        players.add(player);
+        return players;
+    }
+
+}
