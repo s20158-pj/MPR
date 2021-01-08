4c8f379bb371bc8ff2b5dead3a1e50f323550937
Patryk Klimek <s20158@pjwstk.edu.pl>
2021-01-08 17:01:23 +0100

    Added mockito tests for PlayerService

---
+++ gameplay/src/test/java/pl/pjatk/gameplay/player/service/PlayerServiceTest.java
@@ -0,0 +1,90 @@
+package pl.pjatk.gameplay.player.service;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import pl.pjatk.gameplay.player.model.Player;
+import pl.pjatk.gameplay.player.repository.PlayerRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PlayerServiceTest {
+
+    @Mock
+    private PlayerRepository playerRepository;
+
+    @InjectMocks
+    private PlayerService playerService;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all here");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each here");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all here");
+    }
+
+    @Test
+    void isFindAllEmpty() {
+        // given
+        when(playerRepository.findAll()).thenReturn(List.of(new Player()));
+        // when
+        List<Player> all = playerService.findAll();
+        // then
+        assertThat(all).isEmpty();
+    }
+
+    @Test
+    void findById() {
+        // given
+        when(playerRepository.findById(1L)).thenReturn(Optional.of(new Player()));
+        // when
+        Optional<Player> player = playerRepository.findById(1L);
+        // then
+        assertThat(player).isNotEmpty();
+    }
+
+    @Test
+    void isSaved() {
+        //given
+        Player player = new Player("nick", 1, 2, 3);
+        Player player2 = new Player(1L, "nick", 1, 2, 3);
+        when(playerRepository.save(player)).thenReturn(player2);
+        //when
+        Player savedPlayer = playerRepository.save(player);
+        //then
+        assertThat(savedPlayer.getId()).isEqualTo(player2.getId());
+    }
+
+    @Test
+    void wasDeleted() {
+        //when
+        playerRepository.deleteById(1L);
+        //then
+        verify(playerRepository, times(1)).deleteById(1L);
+    }
+
+
+}
